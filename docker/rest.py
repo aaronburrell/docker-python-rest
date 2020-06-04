ab603767fdbeb5df444366f9dbd38bf60a3a9150
Aaron Burrell <aaronburrell@gmail.com>
2020-06-03 22:12:59 -0400

    updated to python 3 with issues

--- docker/rest.py
+++ docker/rest.py
@@ -14,17 +14,17 @@ app = web.application(urls, globals())
 
 class list_users:
     def GET(self):
-	output = 'users:[';
-	for child in root:
-                print 'child', child.tag, child.attrib
-                output += str(child.attrib) + ','
-	output += ']';
-        return output
+	    output = 'users:[';
+	    for child in root:
+            print 'child', child.tag, child.attrib
+            output += str(child.attrib) + ','
+	        output += ']';
+    return output
 
 class get_user:
     def GET(self, user):
-	for child in root:
-		if child.attrib['id'] == user:
+	    for child in root:
+		    if child.attrib['id'] == user:
 		    return str(child.attrib)
 
 if __name__ == "__main__":
